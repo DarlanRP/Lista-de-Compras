4b6f202ca6ab7403af279a31347b7544414dfb24
DarlanRP <darlanrodriguesp97@hotmail.com>
2021-12-07 15:06:01 -0300

    Finaly

---
+++ src/App.styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    background: #17181f;
+    color: #f0f0f0;
+    min-height: 100vh;
+`;
+
+export const Area = styled.div`
+    margin: auto;
+    max-width: 61.25rem;
+    padding: .625rem;
+`;
+
+export const Header = styled.header`
+    margin: 0;
+    padding: 0;
+    color: #f0f0f0;
+    text-align: center;
+    border-bottom: 1px solid #444;
+
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Item } from './types/Item'
+import { ListItem } from './components/ListItem'
+import { AddAndRemove } from './components/AddAndRemove'
+import * as C from './App.styles'
 
 function App() {
+  const [list, setList] = useState<Item[]>([
+
+
+  ])
+
+  const handleAddList = (listName: string) => {
+    let newList = [...list]
+    newList.push({
+      id: list.length + 1,
+      name: listName,
+      done: false,
+    })
+    setList(newList)
+  }
+
+  const handleList = (id: number, done: boolean) => {
+    let newList = [...list]
+    for (let i in newList) {
+      if (newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <C.Container>
+      <C.Area>
+        <C.Header><h1> Lista </h1></C.Header>
+
+        <AddAndRemove onEnter={handleAddList} />
+
+
+
+        {list.map((item, index) => (
+          <ListItem
+            key={index}
+            item={item}
+            onChange={handleList}
+          />
+        ))}
+
+      </C.Area>
+    </C.Container>
+  )
 }
 
 export default App;
